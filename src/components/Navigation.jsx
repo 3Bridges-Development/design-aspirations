4596986f9682cd20e8d176cce5ac5e8b32d01eb1
nkleinmann <nekleinmann@gmail.com>
2023-03-12 16:12:19 -0400

    add navigation, update tailwind, and add views

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navigation() {
+    return (
+        <>
+            <section className="flex justify-center">
+                <div className="flex flex-col justify-around flex-wrap md:flex-row text-da-burgundy w-10/12">
+                    <div>HOME</div>
+                    <div>ABOUT</div>
+                    <div>INSTRUCTIONAL DESIGN</div>
+                    <div>MULTI-MEDIA DESIGN</div>
+                    <div>DA PERSPECTIVE</div>
+                    <div>CONTACT US</div>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ src/views/MultiMediaDesign.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MultiMediaDesign() {
+    return (
+        <>
+        <div>MMD!</div>
+        </>
+    )
+}
+
+export default MultiMediaDesign
