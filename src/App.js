e9a81bdf0f38d2d31124358ed92ceb2c9ced028d
ambertrand <alex.m.bertrand@gmail.com>
2023-03-12 21:17:49 -0400

    setting up contentful hook w/error handling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
-import Navigation from './components/Navigation';
+import React from "react";
+import Navigation from "./components/Navigation";
 import { Outlet } from "react-router-dom";
+import useContentful from "./hooks/use-contentful";
+import "./App.css";
+
+const query = `
+query {}
+`;
 
 function App() {
+  let { data, errors } = useContentful(query);
+
+  if (errors)
+    return (
+      <span style={{ color: "red" }}>
+        {errors.map((error) => error.message).join(",")}
+      </span>
+    );
+  if (!data) return <span>Loading...</span>;
+
   return (
     <div className="App">
       <Navigation />
