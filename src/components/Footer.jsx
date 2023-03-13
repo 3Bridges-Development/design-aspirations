be7afdb4d2ea6b4ee8c7dd0f92b10f73f3d70847
nkleinmann <nekleinmann@gmail.com>
2023-03-12 22:03:18 -0400

    add home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="flex justify-center space-x-5 flex-wrap flex-row bg-da-light-grey mt-5 p-6 text-lg">
+                <div><a href="https://www.facebook.com/DesignedAspirationsLLC" className="hover:text-blue-600">Facebook</a></div>
+                <div><a href="https://www.instagram.com/designedaspirations/" className="hover:text-blue-600">Instagram</a></div>
+                {/* check with Donisa, this needs to be updated */}
+                <div><a href="https://www.facebook.com/DesignedAspirationsLLC" className="hover:text-blue-600">Tik Tok</a></div>
+            </footer>
+        </>
+    )
+}
---
+++ src/components/WelcomeBanner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WelcomeBanner() {
+    return (
+        <>
+            <div className="flex justify-center flex-wrap bg-da-black text-da-white p-2">WELCOME TO DESIGNED ASPIRATIONS</div>
+        </>
+    )
+}
