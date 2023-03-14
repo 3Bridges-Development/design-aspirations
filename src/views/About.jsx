b146976e419959d93a1477244c8f552d3f056d56
nkleinmann <nekleinmann@gmail.com>
2023-03-13 21:25:40 -0400

    add about and update nav

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -3,7 +3,24 @@ import React from "react"
 function About() {
     return (
         <>
-            <div>About!</div>
+            <section>
+                <h1 className="text-xl flex justify-center pb-4">ABOUT US</h1>
+                <div className="bg-da-mauve p-20 flex flex-col items-center text-center">
+                    <h2 className="text-2xl">“The future belongs to those who believe in the beauty of their dreams.”</h2>
+                    <h3 className="text-xl">Eleanor Roosevelt</h3>
+                </div>
+                <div className="bg-da-black md:px-8 p-2 pt-8 pb-8 flex flex-col items-center">
+                    <h2 className="text-da-white w-3/5 text-xl">Our Mission</h2>
+                    <h2 className="text-da-white w-3/5 text-lg pt-4">At Designed Aspirations, we are committed to helping others Aspire to Be; Aspire to Do; Aspire to Learn, Aspire to Achieve, and Aspire to Dream.</h2>
+                </div>
+                <div className="bg-da-black md:px-8 p-2 pb-8 flex flex-col items-center">
+                    <h2 className="text-da-white w-3/5 text-xl">Our Motivation</h2>
+                    <h2 className="text-da-white w-3/5 text-lg pt-4">Designed Aspirations start may sound familiar. At some point in life, many find themselves unsure of their purpose. Reflecting on experiences, hosts of skillsets, achievements, and what felt the most fulfilling in their career.</h2>
+                    <h2 className="text-da-white w-3/5 text-lg pt-4">I realized that I've always been passionate about wellness, education, and training personally and professionally. That realization, coupled with a sincere desire to help people, is how Designed Aspirations came to be.</h2>
+                    <h2 className="text-da-white w-3/5 text-lg pt-4">Our commitment is to partner with you to assess your needs and lend our expertise in how we learn best, keeping human-centered design in mind.</h2>
+                    <h2 className="text-da-white w-3/5 text-lg pt-4">We will meet you where you are and strategize to get you where you want to be. After an initial consultation and a thorough analysis, you may seek a detailed report to start conversations within your organization. You may be looking to upscale your existing material. You may be an expert in your field and are ready to share that knowledge, but you aren't quite sure how. We can help with that, too, by designing and developing effective and engaging solutions.</h2>
+                </div>
+            </section>
         </>
     )
 }
