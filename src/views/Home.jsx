66a43e3666a8e9d2586569a5842de9fd574f93b5
nkleinmann <nekleinmann@gmail.com>
2023-03-13 20:57:25 -0400

    ui updates after pr

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
                 <img src={daLogo} alt="design aspirations logo" className="md:w-2/5 sm:2-1/5 max-h-fit" />
                 {/* check what the functionality of this button should be, I think it should go to calendly link */}
                 <div className="flex items-center">
-                    <button className="md:absolute md:bottom-30 bg-da-black text-da-yellow p-2 max-h-10 hover:text-blue-200" type="button" onClick={console.log("clicked!")}>BOOK CONSULTATION</button>
+                    <button className="md:absolute md:bottom-30 bg-da-black text-da-yellow p-2 m-2 max-h-10 hover:text-blue-200" type="button" onClick={console.log("clicked!")}>BOOK CONSULTATION</button>
                 </div>
                 <hr className="p-1 mt-8 bg-da-black w-4/5 rounded-md"></hr>
             </section>
-            <section className="flex flex-col items-center md:m-0 m-1">
-                <h2 className="text-da-burgundy text-2xl">Designed Aspirations offer Instructional Design and Development of educational and training material.</h2>
+            <section className="flex flex-col items-center text-center mt-1">
+                <h2 className="text-da-burgundy text-2xl justify-center">Designed Aspirations offer Instructional Design and Development of educational and training material.</h2>
                 <h3 className="text-xl m-1">Pharmaceutical - Mental Wellness - Human Services</h3>
             </section>
         </>
