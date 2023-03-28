ff881c535613c1568810813b72af6195466c7dd4
nkleinmann <nekleinmann@gmail.com>
2023-03-27 21:53:03 -0400

    rename image and add card component in ID page

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Card({ image, content, link }) {
+    return (
+        <>
+            <div className="md:w-1/3 flex flex-col items-center pl-8 pr-8 pb-8">
+                <img src={image.src} alt={image.alt} className="h-auto md:w-full w-3/5 pt-8 pb-16 justify-items-center" />
+                <h2 className="text-da-white text-lg">{content.text1}</h2>
+                {content.text2 ? (
+                    <>
+                        <br />
+                        <h2 className="text-da-white text-lg">{content.text2}</h2>
+                    </>
+                ) : (
+                    null
+                )}
+            </div>
+        </>
+    )
+}
--- src/views/InstructionalDesign.jsx
+++ src/views/InstructionalDesign.jsx
@@ -1,11 +1,54 @@
 import React from "react";
 import id_header from "../assets/id_header.png";
+import Card from "../components/Card";
+import ride_share from "../assets/ride_share.png"
 
 function InstructionalDesign() {
     return (
         <>
             <section className="flex justify-center">
-                <img src={id_header} alt="Instructional Design" className="max-h-fit" />
+                <img src={id_header} alt="Instructional Design" className="h-auto md:w-1/5 w-3/5 pt-8 pb-16" />
+            </section>
+            <section className="bg-da-black flex justify-center p-8">
+                <div className="text-da-white w-4/5 pl-8">INSTRUCTIONAL DESIGN</div>
+            </section>
+            <section className="bg-da-black p-8 flex justify-center">
+                {/* need to add .map here instead of hard coding multiple cards */}
+                <div className="flex flex-wrap justify-center w-4/5">
+                    <Card 
+                        image={{ src: ride_share, alt: "Instructional Design"}}
+                        content={{ 
+                            text1: "There is often a misconception as a trainer, coach, instructor, or mental health professional that because you are an expert in your subject matter, you are presenting it in the most engaging and absorbable manner to your audience or client.",
+                            text2: "The role of an Instructional Designer is to be something other than a field expert. It is to take a birdseye view of your established outcomes, ensure that all of your supporting content and activities align with the outcomes, and assess if you can improve how you present it." 
+                        }}
+                        link={{ url: "https://www.google.com/maps" }}
+                    />
+                    {/* remove below cards */}
+                    <Card 
+                        image={{ src: ride_share, alt: "Instructional Design"}}
+                        content={{ 
+                            text1: "There is often a misconception as a trainer, coach, instructor, or mental health professional that because you are an expert in your subject matter, you are presenting it in the most engaging and absorbable manner to your audience or client.",
+                            text2: "The role of an Instructional Designer is to be something other than a field expert. It is to take a birdseye view of your established outcomes, ensure that all of your supporting content and activities align with the outcomes, and assess if you can improve how you present it." 
+                        }}
+                        link={{ url: "https://www.google.com/maps" }}
+                    />
+                    <Card 
+                        image={{ src: ride_share, alt: "Instructional Design"}}
+                        content={{ 
+                            text1: "There is often a misconception as a trainer, coach, instructor, or mental health professional that because you are an expert in your subject matter, you are presenting it in the most engaging and absorbable manner to your audience or client.",
+                            text2: "The role of an Instructional Designer is to be something other than a field expert. It is to take a birdseye view of your established outcomes, ensure that all of your supporting content and activities align with the outcomes, and assess if you can improve how you present it." 
+                        }}
+                        link={{ url: "https://www.google.com/maps" }}
+                    />
+                    <Card 
+                        image={{ src: ride_share, alt: "Instructional Design"}}
+                        content={{ 
+                            text1: "There is often a misconception as a trainer, coach, instructor, or mental health professional that because you are an expert in your subject matter, you are presenting it in the most engaging and absorbable manner to your audience or client.",
+                            text2: "The role of an Instructional Designer is to be something other than a field expert. It is to take a birdseye view of your established outcomes, ensure that all of your supporting content and activities align with the outcomes, and assess if you can improve how you present it." 
+                        }}
+                        link={{ url: "https://www.google.com/maps" }}
+                    />
+                </div>
             </section>
         </>
     )
