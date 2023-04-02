4f3ceaf947f35286507fdc7a40b4247c613277a9
ambertrand <alex.m.bertrand@gmail.com>
2023-04-01 21:14:55 -0400

    move line break to own hook file

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,28 +1,9 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
+import returnLineBreaks from "../hooks/line-break";
 
 function About() {
   const data = useOutletContext();
-  console.log(data);
-
-  const returnLineBreaks = (text) => {
-    if (text.includes("<br/>")) {
-      return text.split("<br/>").map((innerText, i) => {
-        const lineBreakStyle = {
-          display: "block",
-          minHeight: "24px",
-        };
-
-        return (
-          <span key={i} style={lineBreakStyle}>
-            {innerText}
-          </span>
-        );
-      });
-    } else {
-      return text;
-    }
-  };
 
   return (
     <>
