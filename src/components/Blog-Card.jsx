40259ab2b41c4284e6dc4574ffd3d76a52506ecd
nkleinmann <nekleinmann@gmail.com>
2023-06-19 20:25:24 -0400

    add error handling for blog, card, and daPerspective and add markdown to blog card

--- src/components/Blog-Card.jsx
+++ src/components/Blog-Card.jsx
@@ -3,26 +3,32 @@ import React from "react";
 export default function BlogCard({ image, content, onClick }) {
   return (
     <>
-      <div className="flex flex-col items-center overflow-hidden pl-8 pr-8 pb-8 lg:w-2/5 w-4/5">
+      <div className="lg:w-1/3 w-4/5 flex flex-col items-center lg:pl-8 lg:pr-8 pb-8 pt-4 pl-2 pr-2">
         <div className="overflow-hidden flex h-72">
-          <img
-            src={image.src}
-            alt={image.alt}
-            className="h-72 pt-8 pb-12 overflow-hidden justify-items-center"
-          />
+          {image.src && image.alt ? (
+            <img
+              src={image.src}
+              alt={image.alt}
+              className="h-auto pt-8 pb-12 overflow-hidden justify-items-center"
+            />
+          ) : (
+            ""
+          )}
         </div>
-        <h2
-          className="text-lg self-start hover:text-blue-600"
-          onClick={onClick}
-        >
-          {content.text1}
-        </h2>
-        {content.text2 ? (
+        {content.text1 && content.text2 ? (
           <>
-            <br />
-            <h2 className="text-lg self-start">{content.text2}</h2>
-          </>
-        ) : null}
+            <h2
+              className="text-lg self-start hover:text-blue-600"
+              onClick={onClick}
+            >
+              {content.text1}
+            </h2>
+              <br />
+              <h2 className="text-lg self-start">{content.text2}</h2>
+            </>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
